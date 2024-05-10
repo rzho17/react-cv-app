28d78a4e042c0299e8769efedff739ddb74730e7
Richard <richardzehungho@gmail.com>
2024-05-10 15:30:45 -0700

    changed route to img files in button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,8 @@
 import "../styles/Button.css";
 
 export default function Button({ func, text, img, name }) {
-  const imageSrc = `../src/assets/${img}`;
+  // const imageSrc = `../src/assets/${img}`;
+  const imageSrc = `https://animated-daifuku-ead10e.netlify.app/src/assets/${img}`;
   return (
     <button className={name} onClick={func}>
       {text}
