cbf6ca41281840fec50f9a2b0333a17cc45aa035
Richard <richardzehungho@gmail.com>
2024-05-13 10:54:40 -0700

    Modified img link src

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,7 @@
 import "../styles/Button.css";
 
 export default function Button({ func, text, img, name }) {
-  // const imageSrc = `../src/assets/${img}`;
-  const imageSrc = `https://animated-daifuku-ead10e.netlify.app/src/assets/${img}`;
+  const imageSrc = `../src/assets/${img}`;
   return (
     <button className={name} onClick={func}>
       {text}
