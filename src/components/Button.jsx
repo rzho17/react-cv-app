ef5e1b9e9ef2fdc8e2d3ea78242a22dea5d6bdb5
Richard <richardzehungho@gmail.com>
2024-05-13 11:21:45 -0700

    changed img file src

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import "../styles/Button.css";
 
 export default function Button({ func, text, img, name }) {
-  const imageSrc = `../public/assets/${img}`;
+  const imageSrc = `/assets/${img}`;
   return (
     <button className={name} onClick={func}>
       {text}
